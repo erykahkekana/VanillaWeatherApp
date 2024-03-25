0d74a17c354e7c7cde536fbf147fd3ef5d0eae2e
Mogau Erykah <kekanaerykah@gmail.com>
2024-03-25 05:29:16 +0200

    more weather data, date and time added with api

--- src/index.js
+++ src/index.js
@@ -3,10 +3,36 @@ function refreshWeather(response) {
     let temperature = response.data.temperature.current
 
     let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = response.data.city;
-
-    temperatureElement.innerHTML = Math.round(temperature);
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
     
+    
+    cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+    temperatureElement.innerHTML = Math.round(temperature);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windElement.innerHTML = `${response.data.wind.speed} km/h`;
+}
+
+function formatDate(date) {
+    
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday"];
+    
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    return `${day} ${hours}:${minutes}`;
 }
 
 
