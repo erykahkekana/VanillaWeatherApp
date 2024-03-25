df925b9f710a2267278a808c611585abc2690638
Mogau Erykah <kekanaerykah@gmail.com>
2024-03-25 04:24:14 +0200

    search functionality with api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current
+
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+
+    temperatureElement.innerHTML = Math.round(temperature);
+    
+}
+
+
+function searchCity(city) {
+    //api call and interface update
+    let apiKey = "7a7f9a4b918ob1073c632f5t4cf473d8";
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiURL).then(refreshWeather);
+}
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML =searchInput.value;
+   
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
+
+searchCity("Polokwane");
 
 //API INTEGRATION
